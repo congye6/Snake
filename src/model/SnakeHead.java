ba76523407675965c018414f1da0becfb12b0324
congye6 <244053679@qq.com>
2015-08-17 15:02:13 +0800

    8.17.3
    delete tail

--- src/model/SnakeHead.java
+++ src/model/SnakeHead.java
@@ -1,16 +1,39 @@
 package model;
 
-public class SnakeHead extends SnakePO{
+public class SnakeHead {
+	private Point point;
+	private Direction direction;
+	
 	
 	public SnakeHead(Point head, Direction headDirection) {
-		super();
 		this.point = head;
 		this.direction = headDirection;
 	}
 	
+	public void right(){
+		point.addX();
+	}
+	
+	public void up(){
+		point.addY();
+	}
+	
+	public void down(){
+		point.minusY();
+	}
+	
+	public void left(){
+		point.minusX();
+	}
+	
+	
 	
 	public void setDirection(Direction headDirection) {
 		this.direction = headDirection;
+	}
+
+	public Direction getDirection() {
+		return direction;
 	}
 	
 	
--- src/model/SnakePO.java
+++ src/model/SnakePO.java
@@ -1,33 +1,15 @@
 package model;
-
 public class SnakePO {
 
-	protected Point point;
-	protected Direction direction;
+	private Point point;
 	
-	public void right(){
-		point.addX();
+	public SnakePO(Point point) {
+		super();
+		this.point = point;
 	}
-	
-	public void up(){
-		point.addY();
-	}
-	
-	public void down(){
-		point.minusY();
-	}
-	
-	public void left(){
-		point.minusX();
-	}
-	
-	public Direction getDirection() {
-		return direction;
-	}
+
 	public Point getPoint() {
 		return point;
 	}
-	public void setDirection(Direction headDirection) {
-		
-	}
+	
 }
